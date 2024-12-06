12f5add625d1d3d6c6b75130ee69873e21dc83e4
Melanie Vida <mvida@castera.io>
2024-12-06 10:31:52 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const port = 3007;
+
+let orderUpdates = [];
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/webhook', (req, res) => {
+  const update = {
+    timestamp: new Date(),
+    ...req.body
+  };
+  orderUpdates.unshift(update);
+  console.log('Received update:', update);
+  res.sendStatus(200);
+});
+
+app.get('/updates', (req, res) => {
+  res.json(orderUpdates);
+});
+
+app.listen(port, () => {
+  console.log(`Webhook server running at http://localhost:${port}`);
+});
